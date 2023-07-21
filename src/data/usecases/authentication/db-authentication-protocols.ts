4c0e876541aef61905120030a7e09580d92326ab
jean.souza3 <jean.souza@ekaizen.digital>
2023-07-21 16:27:44 -0300

    refactor: move DbAuthentication protocols a single file

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,6 @@
+export * from '../../../domain/usecases/authentication'
+export * from '../../protocols/criptography/hash-comparer'
+export * from '../../protocols/db/load-account-by-email-repository'
+export * from '../../protocols/criptography/tokenGenerator'
+export * from '../../protocols/db/update-access-token-repository'
+export * from '../add-account/db-add-account-protocols'
--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,12 @@
-import { AuthenticationModel } from '../../../domain/usecases/authentication'
-import { HashComparer } from '../../protocols/criptography/hash-comparer'
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { AccountModel } from '../add-account/db-add-account-protocols'
 import { DbAuthentication } from './db-authentication'
-import { TokenGenerator } from '../../protocols/criptography/tokenGenerator'
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import {
+  LoadAccountByEmailRepository,
+  AuthenticationModel,
+  HashComparer,
+  AccountModel,
+  TokenGenerator,
+  UpdateAccessTokenRepository
+} from './db-authentication-protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
